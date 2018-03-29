3f8fc93790802214b3daaedc71e436ddb0e1e685
Paul_TORTEL <paul.tortel@efrei.net>
2018-03-29 18:51:15 +0200

    Final push

--- app/src/main/java/spark/museek/MainActivity.java
+++ app/src/main/java/spark/museek/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends Activity
         else
             tryConnectFromPreviousToken();
     }
-
+	
     public void tryConnectFromPreviousToken() {
 
         hasTriedToken = true;
